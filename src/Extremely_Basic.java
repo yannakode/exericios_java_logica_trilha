a395fda162012149a8a101c7de3b9975b490aee4
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-15 19:31:17 -0300

    extremely basic

---
+++ src/Extremely_Basic.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Extremely_Basic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int sum = a + b;
+        System.out.println("X = " + sum);
+    }
+
+}
