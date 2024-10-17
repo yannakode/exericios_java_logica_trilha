12b43043b5852e55041787252afbcf0ebba916f4
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-17 14:12:25 -0300

    password valitador - first soluction, correction in the regex expression

--- src/Passwords_Validator.java
+++ src/Passwords_Validator.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Passwords_Validator {
     public static void main(String[] args) {
-        String pass = "abrBdb1";
-        String [] arr = pass.split("");
-
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$";
+        Scanner sc = new Scanner(System.in);
+        String pass = sc.nextLine();
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,32}$";
         boolean valid = pass.matches(regex);
         System.out.println(valid);
     }
