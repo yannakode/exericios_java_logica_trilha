201c105d2d07ec60a9278e6ec76f735441f52a08
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-16 21:56:54 -0300

    sort_by_length - first solution (burble sort)

---
+++ src/Sort_By_Length.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Sort_By_Length {
+    public static void main(String[] args) {
+        String words = "Top Coder comp Wedn at midnight";
+        sort(words);
+    }
+
+    public static void sort(String word) {
+        String[] words = word.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].length() < words[j].length()) {
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+            }
+        }
+        Arrays.stream(words).forEach(System.out::println);
+    }
+}
