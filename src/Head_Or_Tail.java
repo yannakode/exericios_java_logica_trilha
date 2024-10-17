6dbdf1745f6d0aa23692518e98a278b94fdf1a84
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-17 12:04:05 -0300

    Head or tail complete solution

---
+++ src/Head_Or_Tail.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Head_Or_Tail {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int n = sc.nextInt();
+        int mary = 0;
+        int bob = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int temp = random.nextInt(2);
+            if (temp == 0) {
+                mary++;
+            } else{
+                bob++;
+            }
+        }
+
+        System.out.println("Mary won " + mary  + " times and bob won " + bob + " times");
+    }
+}
