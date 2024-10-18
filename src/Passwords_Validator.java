ea14e67544cad57ed212f779ac504b379695ab4d
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-18 10:26:52 -0300

    Passwords validator - fixed logic errors

--- src/Passwords_Validator.java
+++ src/Passwords_Validator.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 public class Passwords_Validator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String pass = sc.nextLine();
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,32}$";
-        boolean valid = pass.matches(regex);
-        System.out.println(valid);
+        while (sc.hasNextLine()) {
+            String pass = sc.nextLine();
+            String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)[a-zA-Z0-9]{6,32}$";
+
+            boolean valid = pass.matches(regex);
+
+            if (valid) {
+                System.out.println("Senha valida.");
+            } else {
+                System.out.println("Senha invalida.");
+            }
+        }
     }
 }
