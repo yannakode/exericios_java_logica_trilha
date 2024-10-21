ba6aba5521f8451428b205cb510ff10ed0dd78cb
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-21 20:28:42 -0300

    att | square_matrix_ii

--- src/Square_Matrix_II.java
+++ src/Square_Matrix_II.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Square_Matrix_II {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while(true) {
+            int n = sc.nextInt();
+            if(n == 0) break;
+
+            int[][] arr = new int[n][n];
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    arr[i][j] = i + j + 1;
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    System.out.printf("%3d", arr[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
     }
 }
